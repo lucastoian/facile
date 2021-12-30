4158a69837ca890d20b9ca233d873277715e1d33
Voks.Db <dbianchi41w@gmail.com>
2021-12-30 03:56:36 +0100

    pushino di fine anno
    praticamente non ho fatto molto lato database, ho creato le stanze che mancavano e le ho collegate tra di loro
    
    pensavo di rimuovere il tasto "risultati"e di inserire i risultati in panoramica

--- src/PostgreSQL.java
+++ src/PostgreSQL.java
@@ -7,8 +7,13 @@ import java.sql.SQLException;
 
 public class PostgreSQL {
 
-        
+    //#region gestioneCandidati 
+
+    private static final String url = "jdbc:postgresql://ec2-34-255-134-200.eu-west-1.compute.amazonaws.com:5432/du00brnb1t9ok";
+    private static final String user = "ykeiygtowzihlm";
+    private static final String password = "70c908bb89427bd76a11350372a669f02b455e056c3fd572f4a94d1d2b65d48c";
     
+
     public static void writeToDatabase(String name, String surname){
         String url = "jdbc:postgresql://ec2-34-255-134-200.eu-west-1.compute.amazonaws.com:5432/du00brnb1t9ok";
         String user = "ykeiygtowzihlm";
@@ -105,5 +110,63 @@ public class PostgreSQL {
        
         
     }
+
+    //#endregion
+
+    //#region oldLogin
+
+    // public static void registerToDatabase(String nome, String cognome, String passwor, String email, String codFiscale) throws SQLException{
+    //     String url = "jdbc:postgresql://ec2-34-255-134-200.eu-west-1.compute.amazonaws.com:5432/du00brnb1t9ok";
+    //     String user = "ykeiygtowzihlm";
+    //     String password = "70c908bb89427bd76a11350372a669f02b455e056c3fd572f4a94d1d2b65d48c";
+    //     String query = "INSERT INTO utente VALUES(?, ?, ?, ?, ?)";
+
+        
+    //         Connection con = DriverManager.getConnection(url, user, password);
+    //         PreparedStatement pst = con.prepareStatement(query);
+            
+    //         pst.setString(1, nome);
+    //         pst.setString(2, cognome);
+    //         pst.setString(4, passwor);
+    //         pst.setString(3, email);
+    //         pst.setString(5, codFiscale);
+    //         pst.executeUpdate();
+
+    //         System.out.println("UTENTE REGISTRATO CON SUCCESSO");
+        
+
+    // }
+
+
+    // public static boolean login(String email, String passwor) throws SQLException{
+
+    //     String url = "jdbc:postgresql://ec2-34-255-134-200.eu-west-1.compute.amazonaws.com:5432/du00brnb1t9ok";
+    //     String user = "ykeiygtowzihlm";
+    //     String password = "70c908bb89427bd76a11350372a669f02b455e056c3fd572f4a94d1d2b65d48c";
+    //     String query = "SELECT * FROM utente WHERE email = ? AND password = ?";
+
+    //     Connection con = DriverManager.getConnection(url, user, password);
+    //     PreparedStatement pst = con.prepareStatement(query);
+
+    //     pst.setString(1, email);
+    //     pst.setString(2, passwor);
+
+    //     ResultSet result = pst.executeQuery();
+
+    //     while(result.next()){
+    //        return true;
+    //     }
+       
+    //     System.out.println("UTENTE NON TROVATO, REGISTRATI PRIMA");
+    //     return false;
+
+    // }
+
+    //#endregion
+
+    //#region altro
+
+
+    //#endregion
 
 }
